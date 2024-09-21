78a0694be73ac631451c2941f54671754863850a
Sagar Patil <sagarp@perscitussln.com>
2024-09-21 21:34:23 +0530

    set data

--- src/views/admin/marketplace/index.jsx
+++ src/views/admin/marketplace/index.jsx
@@ -64,7 +64,7 @@ export default function Marketplace() {
   };
 
   return (
-    <Box pt={{ base: '180px', md: '80px', xl: '80px' }}>
+    <Box pt={{ base: '93px', md: '46px', xl: '46px' }}>
       <Grid
         mb="20px"
         gridTemplateColumns={{ xl: 'repeat(3, 1fr)', '2xl': '1fr 0.46fr' }}
@@ -143,6 +143,7 @@ export default function Marketplace() {
         <Flex
           flexDirection="column"
           gridArea={{ xl: '1 / 3 / 2 / 4', '2xl': '1 / 2 / 2 / 3' }}
+          pt={{ base: '40px', md: '46px', xl: '46px' }}
         >
           {Cart.length !== 0 && (
             <Card p="0px">
